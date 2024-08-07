45610028953b440380c2fb4d24570ee7413144ea
danuja01 <it21023378@my.sliit.lk>
2024-08-08 00:43:13 +0530

    Feat: Add ChapterCard and LastActivityCard components

---
+++ src/components/cards/chapter-card.jsx
@@ -0,0 +1,10 @@
+const ChapterCard = () => {
+  return (
+    <div className="p-4 my-4 bg-white rounded-xl" style={{ boxShadow: "0px 0px 3px rgba(0, 0, 0, 0.25)" }}>
+      <h3 className="uppercase text-sm font-semibold">Lecture 01</h3>
+      <p className="text-sm font-light">Comprehensive Guide to Data Structures and Algorithms</p>
+    </div>
+  );
+};
+
+export default ChapterCard;
