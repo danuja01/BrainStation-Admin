8bf9aa77e6cbbfc45c57e32bbdcb3fb98e459538
danuja01 <it21023378@my.sliit.lk>
2024-08-08 01:05:37 +0530

    Refactor: Improve ChapterCard and Main components

--- src/components/cards/chapter-card.jsx
+++ src/components/cards/chapter-card.jsx
@@ -1,6 +1,9 @@
 const ChapterCard = () => {
   return (
-    <div className="p-4 my-4 bg-white rounded-xl" style={{ boxShadow: "0px 0px 3px rgba(0, 0, 0, 0.25)" }}>
+    <div
+      className="p-4 my-4 cursor-pointer bg-white rounded-xl hover:brightness-95 transition-all duration-300"
+      style={{ boxShadow: "0px 0px 3px rgba(0, 0, 0, 0.25)" }}
+    >
       <h3 className="uppercase text-sm font-semibold">Lecture 01</h3>
       <p className="text-sm font-light">Comprehensive Guide to Data Structures and Algorithms</p>
     </div>
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,8 +1,16 @@
+import BottomBar from "@/components/common/bottom-bar";
+
 const Main = () => {
   return (
-    <>
-      <p className="font-inter h-full w-full bg-primary-paper p-2 rounded-br-xl">Home Page</p>
-    </>
+    <div className="flex flex-col h-full">
+      <div className="flex-grow bg-primary-paper p-4 rounded-br-xl">
+        <p className="font-inter h-full w-full">Main Page</p>
+      </div>
+      {/* Bottom bar */}
+      <div className="px-4 py-1">
+        <BottomBar />
+      </div>
+    </div>
   );
 };
 
