7cb43003202608a4a70f009c1dcfde767c9520c3
danuja01 <it21023378@my.sliit.lk>
2024-08-08 00:43:53 +0530

    Refactor: Update side panel icons import paths

--- src/components/common/side-panel.jsx
+++ src/components/common/side-panel.jsx
@@ -1,14 +1,14 @@
-import BookIcon from "../side-panel-icons/book";
-import HomeIcon from "../side-panel-icons/home";
-import QuizzIcon from "../side-panel-icons/quize";
+import BookIcon from "../icons/side-panel-icons/book";
+import HomeIcon from "../icons/side-panel-icons/home";
+import QuizzIcon from "../icons/side-panel-icons/quize";
 
 const Sidebar = () => {
   return (
-    <div className="w-[73px] h-full border-r-2 pt-20 p-2 flex-col gap-4 select-none">
+    <div className="w-[73px] h-full border-r pt-20 p-2 flex-col gap-4 select-none">
       <HomeIcon />
-      <div className="border-b-2 my-2" />
+      <div className="border-b my-2" />
       <BookIcon />
-      <div className="border-b-2 my-2" />
+      <div className="border-b my-2" />
       <QuizzIcon />
     </div>
   );
