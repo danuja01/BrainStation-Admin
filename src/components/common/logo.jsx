3011844461a6c054955d151f045c62cb6ab54e4d
danuja01 <it21023378@my.sliit.lk>
2024-08-08 00:43:33 +0530

    Refactor: logo component to use tailwind-merge for dynamic class names

--- src/components/common/logo.jsx
+++ src/components/common/logo.jsx
@@ -1,8 +1,10 @@
-const Logo = () => {
+import { twMerge } from "tailwind-merge";
+
+const Logo = ({ className }) => {
   return (
-    <div className="flex items-center gap-1">
-      <img src="assets/images/logo.png" alt="BrainStation Logo" />
-      <p className="text-4xl font-josfin-sans font-bold">
+    <div className={twMerge("flex items-center", className)}>
+      <img src="assets/images/logo.png" className="w-11" alt="BrainStation Logo" />
+      <p className="text-2xl font-josfin-sans font-bold">
         Brain<span className="font-jura font-light">Station</span>
       </p>
     </div>
