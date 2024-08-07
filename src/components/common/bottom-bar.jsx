aeff65a52016737977fee2c36c403cb0135dc459
danuja01 <it21023378@my.sliit.lk>
2024-08-08 01:05:22 +0530

    Feat: Add BottomBar component

---
+++ src/components/common/bottom-bar.jsx
@@ -0,0 +1,9 @@
+const BottomBar = () => {
+  return (
+    <div className="bg-white w-full py-2 uppercase text-xs text-gray-400 font-semibold font-josfin-sans">
+      Lecture 01
+    </div>
+  );
+};
+
+export default BottomBar;
